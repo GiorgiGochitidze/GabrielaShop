b57c38f994e4f1879d2f5eb58c9bdb35f7406acb
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2022-10-06 20:26:57 +0400

    modified:   daxmareba.html 	modified:   gamoxmaureba.html 	new file:   helpbot.html 	modified:   index.html 	new file:   js/bot.js 	modified:   kontaqtebi.html 	new file:   style/helpbot.css

---
+++ js/bot.js
@@ -0,0 +1,10 @@
+let helpCommand = ['როგორ ვიყიდო ნივთი', 'როგორ დაგეკონტაქტო'];
+
+const selector = document.querySelector('.text-container');
+
+const inputs = document.querySelector('.text-area');
+
+
+function answer(){
+    
+}
