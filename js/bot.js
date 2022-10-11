7af326756f82e56d7f13c67fd0acd892c66e8cc4
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2022-10-11 19:05:50 +0400

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -1,10 +1,20 @@
-let helpCommand = ['როგორ ვიყიდო ნივთი', 'როგორ დაგეკონტაქტო'];
+const bot_questions = ["how old are you?", "whats your name", "whats your parents name", "i know where you live", "dont look behind :))) "]
 
-const selector = document.querySelector('.text-container');
-
-const inputs = document.querySelector('.text-area');
+let currentQuestion = 0
 
 
-function answer(){
-    
-}
+const chatContainer = document.querySelector('.chat-container')
+const form = document.querySelector('form')
+const input = document.querySelector('textarea')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  chatContainer.innerHTML += `   
+  <div class="chat-box me">${input.value.trim()}</div>`
+  chatContainer.innerHTML += `   
+  <div class="chat-box">${bot_questions[currentQuestion]}</div>`
+  
+  input.value = ""
+  
+  currentQuestion++
+})
