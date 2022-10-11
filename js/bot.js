a1cc83715b9d2e68ed69cde1a83d154f56b9541f
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2022-10-11 20:47:39 +0400

    modified:   helpbot.html 	modified:   js/bot.js 	modified:   style/helpbot.css

--- js/bot.js
+++ js/bot.js
@@ -1,10 +1,35 @@
-let helpCommand = ['როგორ ვიყიდო ნივთი', 'როგორ დაგეკონტაქტო'];
+const bot_questions = ["გამარჯობა", "რაგქვია", "ვინ არის შენი შემქმნელი", "რისთვის ხარ განკუთვნილი", "როდის შეიქქმენი"]
 
-const selector = document.querySelector('.text-container');
+const answers = ["JARVIS: გამარჯობა!!", "JARVIS: მე მქვია ჯარვისი", "JARVIS: ჩემი შემქნელია: გიორგი გოჩიტიძე",
+"JARVIS: მე ვარ შექმნილი იმისთვის რომ დაგეხმაროთ, დაგეხმაროთ ყიდვაში და ა.შ"]
 
-const inputs = document.querySelector('.text-area');
+let currentQuestion = 0
 
 
-function answer(){
-    
+const chatContainer = document.querySelector('.chat-container')
+const form = document.querySelector('form')
+const input = document.querySelector('textarea')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(input.value.trim() == bot_questions[0]){
+      chatContainer.innerHTML += `   
+      <div class="chat-box">${answers[0]}</div>`
 }
+
+if(input.value.trim() == bot_questions[1]){
+    chatContainer.innerHTML += `   
+    <div class="chat-box">${answers[1]}</div>`
+}
+    
+if(input.value.trim() == bot_questions[2] || input.value.trim() == "ვინ შეგქმნა" || input.value.trim() == "შემქმნელი"){
+    chatContainer.innerHTML += `   
+    <div class="chat-box">${answers[2]}</div>`
+}
+
+if(input.value.trim() == bot_questions[3] || input.value.trim() == "რისთვის ხარ" || input.value.trim() == "რა არის შენი დანიშნულება"){
+    chatContainer.innerHTML += `   
+    <div class="chat-box">${answers[3]}</div>`
+}
+
+})
