6c48d88af2a251ea03de236ac8730f0f986b6cb7
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2022-10-18 22:11:36 +0400

    modified:   index.html 	modified:   js/index.js 	modified:   style/index.css

--- js/index.js
+++ js/index.js
@@ -4,3 +4,49 @@ meniusButton.addEventListener('click', () => {
     const meniusCategories = document.querySelector(".menius-buttons-list")
     meniusCategories.classList.toggle('none');
 })
+
+const motherx = document.querySelector('.containerX')
+
+const selectAll = document.querySelector('.number1')
+selectAll.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+        slelectGanxorcieleba.classList.toggle('none')
+    const gatisha = document.querySelector('.off')
+    gatisha.addEventListener('click', () => {
+        slelectGanxorcieleba.classList.toggle('none')
+    })
+})
+
+const selectAll2 = document.querySelector('.number2')
+selectAll2.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+        slelectGanxorcieleba.classList.toggle('none')
+})
+
+const selectAll3 = document.querySelector('.number3')
+selectAll3.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+        slelectGanxorcieleba.classList.toggle('none')
+})
+
+const selectAll4 = document.querySelector('.number4')
+selectAll4.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+        slelectGanxorcieleba.classList.toggle('none')
+    const gatisha = document.querySelector('.off')
+    gatisha.addEventListener('click', () => {
+        slelectGanxorcieleba.classList.toggle('none')
+    })
+})
+
+const selectAll5 = document.querySelector('.number5')
+selectAll5.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+        slelectGanxorcieleba.classList.toggle('none')
+})
+
+const selectAll6 = document.querySelector('.number6')
+selectAll6.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+        slelectGanxorcieleba.classList.toggle('none')
+})
