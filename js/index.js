15eaab0daff697b24d8b765a815a22a679579029
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2022-10-22 16:44:05 +0400

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -8,15 +8,10 @@ meniusButton.addEventListener('click', () => {
 const motherx = document.querySelector('.containerX')
 
 const boxes = document.querySelectorAll('.buy-btn')
-const gatisha = document.querySelector('.off')
 
 boxes.forEach(buybtn => {
-	buybtn.addEventListener('click', () => {
-        const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
-        slelectGanxorcieleba.classList.toggle('none')
-        
-            gatisha.addEventListener('click', () => {
-            slelectGanxorcieleba.classList.toggle('none')
-      })
+  buybtn.addEventListener('click', () => {
+    const slelectGanxorcieleba = document.querySelector('.ganxorcieleba')
+    slelectGanxorcieleba.classList.toggle('none')
     })
   })
